82d376cd6357deae4f6ddc6604e727479ffa3f93
Aditya Bakare <adityab7@protonmail.com>
2020-10-15 22:38:19 +0530

    lib/blocs: Create Weather Bloc
    Signed-off-by: Aditya Bakare <adityab7@protonmail.com>

---
+++ lib/blocs/weather_bloc.dart
@@ -0,0 +1,27 @@
+import 'package:meta/meta.dart';
+import 'package:bloc/bloc.dart';
+
+import 'package:weatherapp/repositories/repositories.dart';
+import 'package:weatherapp/models/models.dart';
+import 'package:weatherapp/blocs/blocs.dart';
+
+class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
+  final WeatherRepository weatherRepository;
+
+  WeatherBloc({@required this.weatherRepository})
+      : assert(weatherRepository != null),
+        super(WeatherInitial());
+
+  @override
+  Stream<WeatherState> mapEventToState(WeatherEvent event) async* {
+    if (event is WeatherRequested) {
+      yield WeatherLoadInProgress();
+      try {
+        final Weather weather = await weatherRepository.getWeather(event.city);
+        yield WeatherLoadSuccess(weather: weather);
+      } catch (_) {
+        yield WeatherLoadFailure();
+      }
+    }
+  }
+}
